ede7ebce56c01a446070df38f93fa33cc2ce5210
Dario Vargas <93060452+DARVAR2K@users.noreply.github.com>
2024-06-05 11:00:46 -0600

    create - page form name

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,12 +1,20 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Icon } from "react-native-elements";
+<<<<<<< Updated upstream
 import { Senderos } from "../screens/Senderos";
 import { Rutas } from "../screens/Rutas";
 import { Ajustes } from "../screens/Ajustes";
+=======
+import {Senderos} from "../screens/Senderos";
+import {Rutas} from "../screens/Rutas";
+import {Ajustes} from "../screens/Ajustes";
+import { FormularioNombre } from "../screens/FormularioNombre";
+>>>>>>> Stashed changes
 
 
 
 const Tab = createBottomTabNavigator();
+<<<<<<< Updated upstream
 
 export function AppNavigation() {
     return (
@@ -26,6 +34,32 @@ function TabscreenOptions(route, color, size) {
     let iconName;
 
     if (route.name === "Senderos") {
+=======
+ 
+export function AppNavigation(){ 
+    return ( 
+        <Tab.Navigator 
+        screenOptions={({ route }) => ({ 
+            tabBarStyle: { backgroundColor: "#2b2b2b" },
+            tabBarActiveTintColor: "#00a680", 
+            tabBarInactiveTintColor: "#646464", 
+            tabBarIcon: ({ color, size }) => TabscreenOptions(route, color, size),
+                })}
+            > 
+            <Tab.Screen name="Senderos" component={FormularioNombre} options={{title: "Senderos"}}/>
+            <Tab.Screen name="Rutas" component={Rutas} options={{title: "Rutas"}}/>
+            <Tab.Screen name="Ajustes" component={Ajustes} options={{title: "Ajustes"}}/>
+        </Tab.Navigator> 
+    ); 
+ 
+} 
+ 
+function TabscreenOptions(route, color, size) { 
+ 
+    let iconName; 
+   
+    if (route.name==="Senderos") { 
+>>>>>>> Stashed changes
         iconName = "compass";
     }
     if (route.name === "Rutas") {
