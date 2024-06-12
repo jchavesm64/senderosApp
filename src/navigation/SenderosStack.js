9ce90a93bb547db16a701cf87981621c9e65b86a
JesusAbarcaRodriguez <121053502+JesusAbarcaRodriguez@users.noreply.github.com>
2024-06-12 12:21:39 -0600

    [CREATE SenderosStack.js]
    Se creo para poder navegar internamente en las pantallas -> Senderos, Sectores e SectoresInfo

---
+++ src/navigation/SenderosStack.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Senderos } from '../screens/Senderos';
+import { Sectores } from '../screens/Sectores';
+import { SectoresInfo } from '../screens/SectoresInfo';
+const Stack = createStackNavigator();
+
+export function SenderosStack() {
+  return (
+    <Stack.Navigator initialRouteName='SenderosScreen'>
+      <Stack.Screen
+        name="SenderosScreen"
+        component={Senderos}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Sectores"
+        component={Sectores}
+        options={{ title: 'Sectores' }}
+      />
+      <Stack.Screen
+        name="SectoresInfo"
+        component={SectoresInfo}
+        options={{ title: 'Detalles' }}
+      />
+    </Stack.Navigator>
+  );
+}
