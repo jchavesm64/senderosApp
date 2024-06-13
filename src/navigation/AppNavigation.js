fa2104afcbfd19fdbd050de1721434b8bbc4a844
Wander1521 <evander.jimenez.jimenez@est.una.ac.cr>
2024-06-13 03:39:28 -0600

    New version of Scroll weel picker

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,43 +1,45 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Icon } from "react-native-elements";
-import {Senderos} from "../screens/Senderos";
-import {Rutas} from "../screens/Rutas";
-import {Ajustes} from "../screens/Ajustes";
-import { FormularioNombre } from "../screens/FormularioNombre";
-
-
+import { Senderos } from "../screens/Senderos";
+import { Rutas } from "../screens/Rutas";
+import { Ajustes } from "../screens/Ajustes";
+import { ChoiceNumPersons } from "../screens/ChoiceNumPersons";
 
 const Tab = createBottomTabNavigator();
- 
-export function AppNavigation(){ 
-    return ( 
-        <Tab.Navigator 
-        screenOptions={({ route }) => ({ 
-            tabBarStyle: { backgroundColor: "#2b2b2b" },
-            tabBarActiveTintColor: "#00a680", 
-            tabBarInactiveTintColor: "#646464", 
-            tabBarIcon: ({ color, size }) => TabscreenOptions(route, color, size),
-                })}
-            > 
-            <Tab.Screen name="Senderos" component={FormularioNombre} options={{title: "Senderos"}}/>
-            <Tab.Screen name="Rutas" component={Rutas} options={{title: "Rutas"}}/>
-            <Tab.Screen name="Ajustes" component={Ajustes} options={{title: "Ajustes"}}/>
-        </Tab.Navigator> 
-    ); 
- 
-} 
- 
-function TabscreenOptions(route, color, size) { 
- 
-    let iconName; 
-   
-    if (route.name==="Senderos") { 
+
+export function AppNavigation() {
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarStyle: { backgroundColor: "#2b2b2b" },
+                tabBarActiveTintColor: "#00a680",
+                tabBarInactiveTintColor: "#646464",
+                tabBarIcon: ({ color, size }) => TabscreenOptions(route, color, size),
+            })}
+        >
+            <Tab.Screen name="Personas" component={ChoiceNumPersons} options={{ title: "Personas" }} />
+            <Tab.Screen name="Senderos" component={Senderos} options={{ title: "Senderos" }} />
+            <Tab.Screen name="Rutas" component={Rutas} options={{ title: "Rutas" }} />
+            <Tab.Screen name="Ajustes" component={Ajustes} options={{ title: "Ajustes" }} />
+        </Tab.Navigator>
+    );
+
+}
+
+function TabscreenOptions(route, color, size) {
+
+    let iconName;
+
+    if (route.name === "Senderos") {
         iconName = "compass";
     }
     if (route.name === "Rutas") {
         iconName = "map";
     }
     if (route.name === "Ajustes") {
+        iconName = "cogs";
+    }
+    if (route.name === "Personas") {
         iconName = "cogs";
     }
 
---
+++ src/screens/Personas.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { ChoiceNumPersons } from "./ChoiceNumPersons"
+
+export function Personas() {
+    return (
+        <View>
+            <Text>Personas</Text>
+            <ChoiceNumPersons />
+        </View>
+    )
+}
